bd1a336b1a56e98f0208e25f5690a94dbee625f0
Daniel Arellano <arellanodanielkeith@gmail.com>
2016-05-09 13:22:51 -0700

    testing travis

--- tests/testAuthorizeMiddleware.js
+++ tests/testAuthorizeMiddleware.js
@@ -9,7 +9,7 @@ describe('Authorize Middleware', function(){
     it('should drop a request to a protected route if no token is given', function(done){
       request(app)
         .post('/api/v1/validate_token')
-        .expect(200, done);
+        .expect(401, done);
     });
 
     it('should drop a request to a protected route if an invalid token is given', function(done){
